0e147f75a99d624b1526e2282a1706597ed23770
Vladislav S <vladislavs@brightdata.com>
2025-10-28 13:21:44 +0200

    add local dev deploy script

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+cp dist/consent.bundle.js ../../screensaver/apps-generator/sources/webos/source/lib/
+cp dist/consent.bundle.js ../../html5-games/apps-games/sources/webos/source/
+cp dist/consent.bundle.js ../../../BrightVideo/webos-app/js/
+cp dist/consent.bundle.js ../../external-consent-wizard/public/lib
