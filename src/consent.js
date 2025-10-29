691d8aa71e91f17d5830d7b1a0dd25bfa46b0c73
Vladislav S <vladislavs@brightdata.com>
2025-10-29 08:16:21 +0200

    fix labels

--- src/consent.js
+++ src/consent.js
@@ -63,8 +63,8 @@ function createConsentModule(targetId, options = {}) {
         footerTextColor: "#777",
         // use translation keys by default (fall back to literal text if key not found)
         benefitText: 'desc_benefit_free',
-        declineButtonText: "accept",
-        acceptButtonText: "decline",
+        declineButtonText: "decline",
+        acceptButtonText: "accept",
         preview: false,
         simpleOptOut: false,
         language: 'en', // Default language
